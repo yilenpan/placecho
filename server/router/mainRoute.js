4664ba59d4c6b1157b4fe0208a1116e8c4f60b0b
Yilen <yilen.pan@gmail.com>
2015-12-29 20:44:55 -0800

    facts

--- server/router/mainRoute.js
+++ server/router/mainRoute.js
@@ -1,8 +1,8 @@
 var checkSize = require('../middleware/checksize');
 var serveFile = require("../controllers/serveFile");
-var proTip = require('../controllers/proTip');
+var fact = require('../controllers/fact');
 
 module.exports = function (app) {
   app.get('/:x/:y', checkSize, serveFile);
-  app.get('/protip', proTip)
+  app.get('/fact', fact)
 };
